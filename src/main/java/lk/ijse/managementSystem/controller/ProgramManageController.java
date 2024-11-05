bd92b4b18972091934ef1eb8c88a9d7d6b45daab
Charith Harsha <charithharshajayashan123@gmail.com>
2024-11-05 20:31:43 +0530

    create student-manage.fxml and program-manage.fxml

--- src/main/java/lk/ijse/managementSystem/controller/ProgramManageController.java
+++ src/main/java/lk/ijse/managementSystem/controller/ProgramManageController.java
@@ -1,4 +1,111 @@
 package lk.ijse.managementSystem.controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class ProgramManageController {
+
+    @FXML
+    private TableColumn<?, ?> colDuration;
+
+    @FXML
+    private TableColumn<?, ?> colFee;
+
+    @FXML
+    private TableColumn<?, ?> colProgramId;
+
+    @FXML
+    private TableColumn<?, ?> colProgramName;
+
+    @FXML
+    private TableView<?> lblProgram;
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private JFXTextField txtDuration;
+
+    @FXML
+    private JFXTextField txtFee;
+
+    @FXML
+    private JFXTextField txtProgramId;
+
+    @FXML
+    private JFXTextField txtProgramName;
+
+    @FXML
+    private JFXTextField txtSearchProgram;
+
+    @FXML
+    void btnAddButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleleProgramOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateProgramOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtDurationOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtFeeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtFeeOnKeyRelease(KeyEvent event) {
+
+    }
+
+    @FXML
+    void txtProgramIdOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtProgramNameOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtProgramNameOnKeyRelease(KeyEvent event) {
+
+    }
+
+    @FXML
+    void txtProgramOnKeyRelease(KeyEvent event) {
+
+    }
+
+    @FXML
+    void txtProgramSearchOnAction(ActionEvent event) {
+
+    }
+
 }
---
+++ src/main/java/lk/ijse/managementSystem/controller/StudentManageController.java
@@ -0,0 +1,107 @@
+package lk.ijse.managementSystem.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.KeyEvent;
+
+public class StudentManageController {
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colStudentId;
+
+    @FXML
+    private TableColumn<?, ?> colStudentName;
+
+    @FXML
+    private TableColumn<?, ?> colTelephoneNumber;
+
+    @FXML
+    private TableView<?> lblStudent;
+
+    @FXML
+    private JFXTextField txtDuration;
+
+    @FXML
+    private JFXTextField txtFee;
+
+    @FXML
+    private JFXTextField txtSearchStudent;
+
+    @FXML
+    private JFXTextField txtStudentId;
+
+    @FXML
+    private JFXTextField txtStudentName;
+
+    @FXML
+    void btnClearBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleleStudentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnStudentAddButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateStudentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAddressOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtStudentIdOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtStudentNameOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtStudentNameOnKeyRelease(KeyEvent event) {
+
+    }
+
+    @FXML
+    void txtStudentOnKeyRelease(KeyEvent event) {
+
+    }
+
+    @FXML
+    void txtStudentSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtTelOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtTelOnKeyRelease(KeyEvent event) {
+
+    }
+
+}
